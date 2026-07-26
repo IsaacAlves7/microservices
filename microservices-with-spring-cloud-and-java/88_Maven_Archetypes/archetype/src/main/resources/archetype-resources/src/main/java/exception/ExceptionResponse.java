134dd2fa8a3161262756b37ea445238d8bd07b62
Isaac Alves Pinheiro <61624336+IsaacAlves7@users.noreply.github.com>
2026-07-26 18:18:23 -0300

    Delete microservices-with-spring-cloud-and-java directory

--- microservices-with-spring-cloud-and-java/88_Maven_Archetypes/archetype/src/main/resources/archetype-resources/src/main/java/exception/ExceptionResponse.java
+++
@@ -1,8 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.exception;
-
-import java.util.Date;
-
-public record ExceptionResponse(Date timestamp, String message, String details) {}
